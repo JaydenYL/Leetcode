a02dbfe30cd0778eecd571dc15a511a351d115e4
JaydenYL <yuliumic@outlook.com>
2020-09-30 08:12:39 +1000

    Update L209x1.java

--- L209/L209x1.java
+++ L209/L209x1.java
@@ -1,12 +1,10 @@
 class Solution {
 	
-	public static void main(String[] args){
-		int[] ar = new int[]{2,3,1,2,4,3};
-		Solution s = new Solution();
-		s.minSubArrayLen(7, ar);
-	}
-	
+
 	public int minSubArrayLen(int s, int[] nums) {
+		if (nums == null || nums.length == 0){
+			return 0;
+		}
 		if (nums.length == 1){
 			return (nums[0] == s) ? 1 : 0;
 		}
@@ -15,8 +13,8 @@ class Solution {
 		int sum = nums[0];
 		int min_len = nums.length+1;
 		while( rt < nums.length ){
-			System.out.printf("%d, %d, sum = %d\n", lf, rt, sum);
-			if( sum > s) {
+			if( sum >= s) {
+				min_len = Math.min(min_len, rt - lf + 1);
 				if (rt == lf){
 					if(rt == nums.length -1){
 						break;
@@ -26,22 +24,13 @@ class Solution {
 				} else {
 					sum -= nums[lf++];
 				}
-			} else if (sum < s) {
+			} else  {
 				if(rt == nums.length -1){
 					break;
 				}
 				sum += nums[++rt];
 				
-			} else {
-				if( min_len > rt - lf + 1 ){
-					min_len = rt - lf + 1;
-				}
-				if (rt == nums.length -1){
-					break;
-				}
-				
-				sum += (nums[++rt] - nums[lf++]);
-			}
+			} 
 		}
 		return (min_len == nums.length+1) ? 0 : min_len;
 	}
